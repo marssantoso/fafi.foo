20feec5d4a5e6d3290dc8c05709fe38b009f1d3d
Mars Santoso <mars.project@outlook.com>
2023-11-06 15:29:16 +0700

    change logic to card generation

--- src/games/century/constants.ts
+++ src/games/century/constants.ts
@@ -1,15 +1,12 @@
 import type { ActionCard, Card, Gems } from './types'
-import { randomizeCard, randomizeActionCard } from './utils'
+import { generateActionCards, generateCards } from './utils'
 
 export const PRICE_WEIGHT_MAP: Gems = [2, 3, 4, 5]
 
 export const ACTION_TYPES: Array<'gain' | 'exchange' | 'upgrade'> = ['gain', 'exchange', 'upgrade']
 
-export const CARDS: Card[] = Array.from(Array(20)).map(randomizeCard)
+export const CARDS: Card[] = generateCards(10)
 
-export const ACTION_CARDS: ActionCard[] = Array.from(Array(30)).map(randomizeActionCard)
+export const ACTION_CARDS: ActionCard[] = generateActionCards(20)
 
-export const STARTER_ACTION_CARDS: [ActionCard, ActionCard] = [
-  { gain: [2, 0, 0, 0] },
-  { upgrade: 2 },
-]
+export const STARTER_ACTION_CARDS: [ActionCard, ActionCard] = [{ gain: [2, 0, 0, 0] }, { upgrade: 2 }]
--- src/games/century/utils.ts
+++ src/games/century/utils.ts
@@ -1,36 +1,63 @@
 import { ActionCard, Card, Gems } from './types.ts'
 import { PRICE_WEIGHT_MAP, ACTION_TYPES } from './constants'
 
-const totalGemPoint = (gems: Gems) => gems.reduce((a, b, j) => a + b * PRICE_WEIGHT_MAP[j], 0)
+const getRandomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min
 
-// result sum must be <= 10
-// result total point must be <= point
-// point must be 6 - 20
-const randomizeGems = (point: number): Gems =>
-  PRICE_WEIGHT_MAP.reduce(
-    (p, w, i) => {
-      const currGems: Gems = [...p]
-      const currentSum = currGems.reduce((a, b) => a + b)
-      const currentTotal = totalGemPoint(currGems)
-      const remainingMax = Math.min(10 - currentSum, Math.floor((point - currentTotal) / w))
-      currGems[i] = Math.round(Math.random() * remainingMax)
-      return currGems
-    },
-    [0, 0, 0, 0]
-  )
+const getRandomGems = (point: number): Gems => {
+  for (;;) {
+    const a = getRandomInt(0, 5);
+    const b = getRandomInt(0, 5);
+    const c = getRandomInt(0, 4);
+    const d = getRandomInt(0, 4);
+
+    const totalGem = a + b + c + d
+    const totalWeight = [a, b, c, d].reduce((a, b, i) => a + b * PRICE_WEIGHT_MAP[i], 0)
+    if (totalGem <= 10 && totalWeight === point) {
+      return [a, b, c, d];
+    }
+  }
+}
 
 export const randomizeCard = (): Card => {
-  const price = randomizeGems(Math.floor(Math.random() * 13) + 8)
-  const point = totalGemPoint(price)
+  const point = getRandomInt(6, 20)
+  const price = getRandomGems(point)
   return { point, price }
 }
 
 export const randomizeActionCard = (): ActionCard => {
-  const actionType = ACTION_TYPES[Math.floor(Math.random() * 3)]
+  const actionType = ACTION_TYPES[getRandomInt(0, 2)]
+
   if (actionType === 'gain') {
-    return { gain: randomizeGems(6) }
+    return { gain: getRandomGems(getRandomInt(4, 8)) }
   }
-  if (actionType === 'upgrade') return { upgrade: Math.round(Math.random() * 2) + 1 }
-  const from = Math.round(Math.random() * 6) + Math.round(Math.random() * 4)
-  return { exchange: [randomizeGems(from), randomizeGems(from + Math.round(Math.random() * 4))] }
+
+  if (actionType === 'upgrade') {
+    return { upgrade: getRandomInt(1, 3) }
+  }
+
+  const from = getRandomInt(getRandomInt(2, 4), getRandomInt(6, 8))
+  const to = getRandomInt(from, getRandomInt(from, from + 14))
+  return { exchange: [getRandomGems(from), getRandomGems(to)] }
+}
+
+export const generateCards = (n: number): Card[] => {
+  const cards: Card[] = []
+  while (cards.length < n) {
+    const card = randomizeCard()
+    if (cards.every((c) => c.price.join('') !== card.price.join(''))) {
+      cards.push(card)
+    }
+  }
+  return cards
+}
+
+export const generateActionCards = (n: number): ActionCard[] => {
+  const cards: ActionCard[] = []
+  while (cards.length < n) {
+    const card = randomizeActionCard()
+    if (cards.every((c) => JSON.stringify(c) !== JSON.stringify(card))) {
+      cards.push(card)
+    }
+  }
+  return cards
 }
