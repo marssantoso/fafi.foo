3f5876693077e2ef6177d8c9af8e1a907321ff7e
Mars Santoso <mars.project@outlook.com>
2023-11-22 03:19:14 +0700

    style: favicon and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,39 @@
 import localForage from 'localforage'
 import { usePlayerName } from '~/hooks/usePlayerName.ts'
 import styles from './header.module.css'
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const [playerName, setPlayerName] = usePlayerName()
 
   const onEnterName = () => {
     const newName = prompt('Enter player name')
+    if (!newName) return
     localForage.setItem('playerName', newName).then(setPlayerName)
   }
 
   return (
     <div className={styles.header}>
-      <Link to="/g" className={styles.brand}>fafi.foo</Link>
-      {playerName ? (
-        <div>
-          <span>Playing as: {playerName} </span>
-          <a href="#" onClick={onEnterName}>
-            (edit)
+      <Link to="/" className={styles.brand}>
+        <img src="/fafi.foo.svg" alt="fafi.foo" width={64} height={64} className={styles.logo} />
+        <span>fafi.foo</span>
+      </Link>
+      <div className={styles.user}>
+        {playerName ? (
+          <>
+            <p className={styles.user__name}>
+              Playing as: <strong>{playerName}</strong>
+            </p>
+            <a onClick={onEnterName} className={styles.user__link}>
+              [edit]
+            </a>
+          </>
+        ) : (
+          <a onClick={onEnterName} className={styles.user__link}>
+            [enter player name]
           </a>
-        </div>
-      ) : (
-        <a href="#" onClick={onEnterName}>
-          enter name
-        </a>
-      )}
+        )}
+      </div>
     </div>
   )
 }
