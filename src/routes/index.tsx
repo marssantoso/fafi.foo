aaf3211532df047c4679cc2850e3893a0506d063
Mars Santoso <mars.project@outlook.com>
2023-11-22 03:37:25 +0700

    style: homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import Header from "~/components/Header";
-// import '../index.css'
-// import './index.css'
+import styles from './index.module.css'
 
 function Root() {
   return (
     <>
       <Header />
-      <h1 className="page__title">fafi.foo</h1>
-      <div style={{ textAlign: 'center' }}>
-        <Link to="/g">Go to Games</Link>
-      </div>
+      <main className={styles.main}>
+        <div className={styles.content}>
+          <img src="/fafi.foo.svg" alt="fafi.foo" width={256} height={256} />
+          <p>A personal collection of turn-based multiplayer board games</p>
+          <Link to="/g">Go to Games</Link>
+        </div>
+      </main>
     </>
   )
 }
